c25b76e123068656ee06c70bf1de382e09031dff
Max Oliphant-Hand <s3663337@student.rmit.edu.au>
2021-04-22 13:10:36 +0930

    Week 6

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,11 @@
 Rails.application.routes.draw do
+  get 'users/login'
+  get 'users/signup'
   get 'static_pages/home'
+  post 'static_pages/home'
   get 'static_pages/help'
-  get 'static_pages/about'
+  get '/about', to: 'static_pages#about'
+  get '/contact', to: 'static_pages#contact'
   get 'hello/index'
   root 'static_pages#home'
 end
